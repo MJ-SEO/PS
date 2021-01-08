b33eb112088e3b6e82fab03994e0a1108b6570fa
MJ-SEO <21600343@handong.edu>
2021-01-08 16:27:47 +0900

    0108

--- backt/nm.c
+++ backt/nm.c
@@ -9,8 +9,21 @@ int main(){
                 arr[i] = i+1;
         }
 
-        int temp =0;
+	int temp = 0;
 
+	do{
+		int t=0;
+		int n=0;
+		while(t!=m){
+			printf("%d ", arr[temp]); n++;
+			if(n==m){
+				printf("\n");
+			}
+			t++;
+		}
+		temp++;
+	}while(temp!=n);
+/*
         for(int i=0; i<n; i++){
                 for(int j=i+1; j<n; j++){
                         for(int k=j+1; k<n; k++){
@@ -22,4 +35,5 @@ int main(){
         }
 
         printf("%d", max);
+*/
 }

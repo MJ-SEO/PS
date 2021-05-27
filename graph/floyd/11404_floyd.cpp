3cfd21a119be337a4c9a203716c1e08efdeb1020
MJ-SEO <21600343@handong.edu>
2021-05-27 20:26:35 +0900

    0527

--- graph/floyd/11404_floyd.cpp
+++ graph/floyd/11404_floyd.cpp
@@ -1,37 +1,52 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
-#include <queue>
+#include <utility>
 
 using namespace std;
-using pii = pair<int, int>
+
+const int INF = 1e9+10;
 
 int map[105][105];
-vector<pii> graph[105];
-
 int main(){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
 	int n, m;
-	cin >> n >> ;
+	cin >> n >> m;
+	for(int i = 1; i <= n; i++)
+		fill(map[i], map[i]+1+n, INF);
 
-	for(int i=0; i<m; i++){
-		int start, end, cost;
-		cin >> start >> end >> cost;
-		graph[start].push_back({cost, end});	
+	while(m--){
+		int a,b,c;
+		cin >> a >> b >> c;
+		map[a][b] = min(map[a][b], c);
 	}
 
-	for(int i=0; i<n-1; i++){
-		floyd();
-	}
+	for(int i = 1; i <= n; i++) map[i][i] = 0;
 
-	for(int i=1; i<=n; i++){
-		for(int j=1; j<=n; j++){
-			cout << map[i][j] << " ";
+	cout << "============INIT============\n";
+	for(int i = 1; i <= n; i++){
+		for(int j = 1; j <= n; j++){
+			if(map[i][j] == INF) cout << "0 ";
+			else cout << map[i][j] << ' ';
 		}
-		cout << "\n";
+		cout << '\n';
 	}
-	return 0;
+	cout << "============================\n";
+
+
+
+	for(int k = 1; k <= n; k++)
+		for(int i = 1; i <= n; i++)
+			for(int j = 1; j <= n; j++)
+				map[i][j] = min(map[i][j], map[i][k]+map[k][j]);
+
+	for(int i = 1; i <= n; i++){
+		for(int j = 1; j <= n; j++){
+			if(map[i][j] == INF) cout << "0 ";
+			else cout << map[i][j] << ' ';
+		}
+		cout << '\n';
+	}
 }
 

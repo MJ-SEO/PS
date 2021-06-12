631a9c3397983fb4e0e1ea64d07ed2517fdd7295
MJ-SEO <21600343@handong.edu>
2021-06-12 22:55:00 +0900

    0612

---
+++ english/3533_EF.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(){
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+	int arr[10];
+	int result = 0;
+
+	for(int i=0; i<10; i++){
+		cin >> arr[i];
+	}
+
+	cout << result << "\n";
+        return 0;
+}
+

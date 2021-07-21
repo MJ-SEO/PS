2089d39b8f0cd725ef82291c7d259124bf886b2d
MJ-SEO <21600343@handong.edu>
2021-07-21 22:58:25 +0900

    0721

---
+++ math/1934_LCM.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int a[50000];
+int b[50000];
+
+int getlcm(int a, int b){
+	int r1 = 0, r2 =0;
+
+	for(int i=0; r1<=45000; i++){
+				
+	}
+
+	for(int i=0; r2<=45000; i++){
+
+	}
+
+	return result;
+}
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	for(int i=0; i<3; i++){
+		int a, b;
+		cin >> a >> b;
+
+		int lcm = getlcm(a, b);
+
+		cout << lcm;
+	}
+
+	return 0;
+}
+

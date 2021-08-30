ecdda4a64e75d886e92913aa74523473d87ac18d
MJ-SEO <21600343@handong.edu>
2021-08-30 17:27:34 +0900

    0830

--- DP/11066_filesum.cpp
+++ DP/11066_filesum.cpp
@@ -4,9 +4,27 @@
 
 using namespace std;
 
+int arr[501];
+int dp[501];
+
 int main(){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+	
+	int t;
+	cin >> t;
+
+	for(int i=0; i<t; i++){
+		int k;
+		cin >> k;
+		memset(arr, 0, sizeof(arr));
+		for(int j=0; j<k; j++){
+			cin >> arr[j];
+		}
+	
+			
+	}
+
 
 	return 0;
 }

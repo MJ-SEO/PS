08c8817295c22f33ead3ae2cdb118d838229cec8
MJ-SEO <audwns392@naver.com>
2022-09-16 17:57:05 +0900

    22_0916

---
+++ recursion/1074_z.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int n, r, c;
+int x, y, result;
+
+void func(int x, int y, int n){
+
+        if(x == r && y == c){
+                cout << result << "\n";
+                return;
+        }
+
+        if(r < n + x && r >= x && c < y+n && c >=y){
+                func(x, y, n/2);
+                func(x, y+n/2, n/2);
+                func(x+n/2, y, n/2);
+                func(x+n/2, y+n/2, n/2);
+        }
+        else{
+                result += n*n;
+        }
+}
+
+
+int main(){
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+
+        cin >> n >> r >> c;
+
+        func(0, 0, (1 << n));
+
+        return 0;
+}
--- recursion/1780_paper.cpp
+++ recursion/1780_paper.cpp
@@ -2,53 +2,38 @@
 
 using namespace std;
 
-int map[2200][2200];
+int map[130][130];
 
-int zero;
-int one;
-int m_one;
+int white;
+int blue;
 
-void func(int x, int y, int size){
+void func(int y, int x, int size){
 	
-	int flag = map[x][y];
+	int flag = map[y][x];
 
-	for(int i=x; i< x+size; i++){	
-		for(int j = y; j< y+size; j++){
-			if(flag == 0 && map[i][j] != 0){
-				flag = 2;
-			
-			}
-			else if(flag == 1 && map[i][j] != 1){
-				flag = 2;
-			
-			}
-			else if(flag == -1 && map[i][j] != -1){
+	for(int i=y; i< y+size; i++){	
+		for(int j = x; j< x+size; j++){
+			if(flag == 0 && map[i][j] == 1){
 				flag = 2;
 			}
-
+			else if(flag == 1 && map[i][j] == 0){
+				flag = 2;
+			}
 			if(flag == 2){
-				func(x, y, size/3);
-				func(x, y + size/3, size/3);
-				func(x, y + (size/3)*2, size/3);
-				func(x + size/3, y, size/3);
-				func(x + size/3, y + size/3, size/3);
-				func(x + size/3, y + (size/3)*2, size/3);
-				func(x + (size/3)*2, y, size/3);
-				func(x + (size/3)*2, y + size/3, size/3);
-				func(x + (size/3)*2, y + (size/3)*2, size/3);
+				func(y, x, size/2);
+				func(y, x + size/2, size/2);
+				func(y + size/2, x, size/2);
+				func(y + size/2, x + size/2, size/2);
 				return;
 			}
 		}
 	}
 
 	if(flag == 0){
-		zero++;
-	}
-	else if(flag == 1){
-		one++;
+		white++;
 	}
 	else{
-		m_one++;
+		blue++;
 	}
 }
 
@@ -56,6 +41,12 @@ void func(int x, int y, int size){
 int main(){
 	int n;
 	cin >> n;
+
+	for(int i=0; i<130; i++){
+		for(int j=0; j<130; j++){
+			map[i][j] = 2;
+		}
+	}
 
 	for(int i=0; i<n; i++){	
 		for(int j=0; j<n; j++){
@@ -65,6 +56,6 @@ int main(){
 
 	func(0, 0, n);
 
-	cout << m_one << "\n" << zero << "\n" << one;
+	cout << white << "\n" << blue << "\n";
 	return 0;
 }
